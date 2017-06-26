c110c26644057e55e2e5055eac6c8bd1082c6930
Elliot Gray <elliotmgray@users.noreply.github.com>
2017-06-26 13:51:32 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import sys
+
+
+if sys.version_info.major != 3:
+    raise RuntimeError("bhtsne requires Python 3")
+
+setup(
+    name="bhtsne",
+    description="Dimensionality Reduction technique",
+    version="2016-03-08",
+    author="Laurens van der Maaten",
+    author_email="lvdmaaten@gmail.com",
+    packages=find_packages(),
+    package_data={
+        '': ['*bhtsne*']
+    },
+    include_package_data=True,
+    zip_safe=False,
+    url="https://github.com/elliotmgray/bhtsne",
+    license="LICENSE",
+    long_description=open("README.md").read(),
+    install_requires=open("requirements.txt").read()
+)
